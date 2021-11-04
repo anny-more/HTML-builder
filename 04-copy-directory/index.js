80cf29bedb4192a0bed7c9fe5cd1a828c0583c28
anny-more <anxxxen@ya.ru>
2021-11-05 00:14:51 +0300

    feat: add functional to 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,14 @@
+const fsPromises = require('fs/promises');
+const fs = require('fs');
+const path = require('path');
+
+fsPromises.mkdir(path.join(__dirname, '/files-copy'), {recursive: true});
+
+fs.readdir(path.join(__dirname, '/files'), (err, files) => {
+    if (err) throw err;
+    
+    for (const file of files) {
+            fsPromises.copyFile(path.join(__dirname, '/files',file), path.join(__dirname, '/files-copy', file));
+        }
+    });
 
